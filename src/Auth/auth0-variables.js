575f491c8b2b069c21467f444895f5e696aa46db
cjimenezv <39573792+cjimenezv@users.noreply.github.com>
2018-08-10 17:21:32 -0500

    Initial commit

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+    domain: 'carlosjimenez.auth0.com',
+    clientId: 'AA58NgZTviVbrlZAYRbfoYfuqaPsq4lx',
+    callbackUrl: 'http://localhost:3000/callback' //Callback URL set in the Auth0 dasbhoard.
+}
